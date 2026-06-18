861dc3e7830c5cf46109f3ad355adc3960bf1c06
dmitryshilin83@gmail.com <dmitryshilin83@gmail.com>
2026-06-18 11:40:22 +0400

    chore: update __init__.py exports, add clean_markdown step to rag_tools full cycle

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -1,6 +1,9 @@
 from .service import ChunkResult, RAGService
+from .search import search_query, format_citation
 
 __all__ = [
     "RAGService",
     "ChunkResult",
+    "search_query",
+    "format_citation",
 ]
